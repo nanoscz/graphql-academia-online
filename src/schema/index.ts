aa26b1108c9073bae058222ee432eead4b03f445
fernando <fernandocto.scz@gmail.com>
2020-03-14 16:41:19 -0400

    [ADD] added schema

---
+++ src/schema/index.ts
@@ -0,0 +1,14 @@
+import "graphql-import-node"
+import { makeExecutableSchema } from "graphql-tools"
+import { GraphQLSchema } from "graphql"
+
+import typeDefs from "./schema.graphql"
+
+import resolvers from "../resolvers/resolversMap"
+
+const schema: GraphQLSchema = makeExecutableSchema({
+  typeDefs,
+  resolvers
+})
+
+export default schema
