996066440cada6d65e9c54d2d8ddf38b9a8b8ec5
fernando <fernandocto.scz@gmail.com>
2020-03-19 20:15:36 -0400

    [ADD] added editCourse

--- src/resolvers/mutation.ts
+++ src/resolvers/mutation.ts
@@ -1,11 +1,13 @@
 import { IResolvers } from 'graphql-tools';
-import { storage } from '../data/data.store';
+import { database } from '../data/data.store';
+import lodash from 'lodash';
+import error from '../handlers/error';
 
 const mutation: IResolvers = {
   Mutation: {
     addCourse(__:void, { course }): any {
       const newCourse = {
-        id: String(storage.coursesJson.length + 1),
+        id: String(database.coursesJson.length + 1),
         title: course.title,
         description: course.description,
         class: course.class,
@@ -16,9 +18,21 @@ const mutation: IResolvers = {
         teacher: course.teacher,
         reviews: [],
       };
-      storage.coursesJson.push(newCourse);
+      database.coursesJson.push(newCourse);
       return newCourse;
     },
+    editCourse(__:void, { course }): any {
+      const elementExits = lodash.findIndex(database.coursesJson, function(o) {
+        return o.id === course.id
+      })
+      if(elementExits === -1) {
+        error(`The course does not exist in the database`);
+      }
+      const reviews = database.coursesJson[elementExits].reviews;
+      course.reviews = reviews;
+      database.coursesJson[elementExits] = course;
+      return course
+    }
   },
 };
 
